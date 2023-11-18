cc39906a745733b35ce5d4b0123af5607c30ae5f
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-11-18 13:08:35 +0000

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Placemark-JPC-V1"
+include(":app")
